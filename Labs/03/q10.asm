c67ae38ef5ea886814936aaea44238d0bca033cc
Hafsa Rashid <142868426+hafsaa05@users.noreply.github.com>
2025-02-15 13:53:44 +0500

    Create q10.asm

---
+++ Labs/03/q10.asm
@@ -0,0 +1,13 @@
+INCLUDE Irvine32.inc
+
+.data
+    bArray BYTE 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 
+           11, 12, 13, 14, 15, 16, 17, 18, 19, 20
+
+.code
+main PROC
+    call DumpRegs   
+    exit
+main ENDP
+
+END main
