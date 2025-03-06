77de5af43fdf758b5f6c5982b7f5bf1e4253ddcc
Hafsa Rashid <142868426+hafsaa05@users.noreply.github.com>
2025-03-06 12:27:02 +0500

    Create word.asm

---
+++ Labs/05/q3/word.asm
@@ -0,0 +1,27 @@
+INCLUDE Irvine32.inc
+
+.data
+    arr dw 61, 43, 11, 52, 25   
+    sorted dw 5 DUP(?)          
+
+.code
+main PROC
+    mov ax, 11  
+    mov [sorted + 0], ax 
+
+    mov ax, 25
+    mov [sorted + 2], ax  
+
+    mov ax, 43
+    mov [sorted + 4], ax 
+
+    mov ax, 52
+    mov [sorted + 6], ax 
+
+    mov ax, 61  
+    mov [sorted + 8], ax 
+
+    call ExitProcess
+main ENDP
+
+END main
