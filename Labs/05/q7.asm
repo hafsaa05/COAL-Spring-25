2bbc5b93c062f092a29306e548f361986faa2370
Hafsa Rashid <142868426+hafsaa05@users.noreply.github.com>
2025-03-06 13:11:37 +0500

    Create q7.asm

---
+++ Labs/05/q7.asm
@@ -0,0 +1,26 @@
+INCLUDE Irvine32.inc
+
+.data
+    arrayB BYTE 60, 70, 80
+    arrayW WORD 150, 250, 350
+    arrayD DWORD 600, 1200, 1800
+
+.code
+main PROC
+    mov esi, OFFSET arrayB
+    mov al, [esi]
+    add al, [esi + 2 * TYPE arrayB]
+
+    mov esi, OFFSET arrayW
+    mov ax, [esi]
+    add ax, [esi + 2 * TYPE arrayW]
+
+    mov esi, OFFSET arrayD
+    mov eax, [esi]
+    add eax, [esi + 2 * TYPE arrayD]
+
+    call DumpRegs
+    call ExitProcess
+main ENDP
+
+END main
