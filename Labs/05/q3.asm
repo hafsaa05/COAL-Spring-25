1344815a319497b86a562251e6769635fdcfd33b
Hafsa Rashid <142868426+hafsaa05@users.noreply.github.com>
2025-03-06 12:22:55 +0500

    Create q3.asm

---
+++ Labs/05/q3.asm
@@ -0,0 +1,27 @@
+INCLUDE Irvine32.inc
+
+.data
+    arr db 61, 43, 11, 52, 25   
+    sorted db 5 DUP(?)          
+
+.code
+main PROC
+    mov al, 11  
+    mov [sorted + 0], al 
+
+    mov al, 25
+    mov [sorted + 1], al 
+
+    mov al, 43
+    mov [sorted + 2], al 
+
+    mov al, 52
+    mov [sorted + 3], al 
+
+    mov al, 61  
+    mov [sorted + 4], al 
+
+    call ExitProcess
+main ENDP
+
+END main
