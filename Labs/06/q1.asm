8f1377acd8fac51a5838315b315daed0d5713de8
Hafsa Rashid <142868426+hafsaa05@users.noreply.github.com>
2025-03-17 00:30:53 +0500

    Update q1.asm

--- Labs/06/q1.asm
+++ Labs/06/q1.asm
@@ -1,1 +1,15 @@
-abc
+include irvine32.inc
+.code
+main proc
+    mov ecx, 10
+    xor eax, eax
+    mov ebx, 1
+    call WriteInt
+L1:
+    xchg eax, ebx
+    add eax, ebx
+    call WriteInt
+    loop L1
+    exit
+main endp
+end main
