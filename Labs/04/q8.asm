1229c90e6b991b05d840dbfdfe668a318e5e92c3
Hafsa Rashid <142868426+hafsaa05@users.noreply.github.com>
2025-03-20 09:02:05 +0500

    Update q8.asm

--- Labs/04/q8.asm
+++ Labs/04/q8.asm
@@ -2,21 +2,21 @@ INCLUDE Irvine32.inc
 
 .data
 val1 BYTE 10h
-val2 WORD 8OOOh
+val2 WORD 8000h
 Val3 DWORD 0FFFFh
 val4 WORD 7FFFh
 
 .code
 main:
     ;i. Write an instruction that increments val2.
-    MOV EAX, val2
+    MOVZX EAX, val2
     INC EAX
 
     ;ii. Write an instruction that subtracts val3 from EAX.
     SUB EAX, Val3
     
     ;iii. Write instructions that subtract val4 from val2.
-    MOV ECX, Val2
+    MOVZX ECX, Val2
     SUB ECX, Val4
 
     call DumpRegs
