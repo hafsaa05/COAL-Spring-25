a15fa685b8bb9fd9605bcf85bfedd54358b47d60
Hafsa Rashid <142868426+hafsaa05@users.noreply.github.com>
2025-04-29 00:30:10 +0500

    Create task5.asm

---
+++ Labs/08/task5.asm
@@ -0,0 +1,45 @@
+INCLUDE Irvine32.inc
+
+.data
+arr WORD 10, 4, 7, 14, 299, 156, 3, 19, 29, 300, 20
+arrSize = 11
+prompt1 BYTE "Enter a number: ", 0
+prompt2 BYTE "Number Found!", 0
+prompt3 BYTE "Number not found!", 0
+
+.code
+main PROC
+    mov edx, OFFSET prompt1
+    call WriteString
+    call ReadInt
+    mov ebx, eax
+
+    mov esi, OFFSET arr
+    mov ecx, arrSize
+
+search_loop:
+    cmp ecx, 0
+    je not_found
+
+    mov ax, [esi]
+    cmp ax, bx
+    je found
+
+    add esi, TYPE arr
+    loop search_loop
+
+not_found:
+    mov edx, OFFSET prompt3
+    call WriteString
+    call Crlf
+    jmp end_program
+
+found:
+    mov edx, OFFSET prompt2
+    call WriteString
+    call Crlf
+
+end_program:
+    exit
+main ENDP
+END main
