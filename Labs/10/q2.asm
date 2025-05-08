749650a6fde9fa634c74f7e27375e9eb0cc326ff
Hafsa Rashid <142868426+hafsaa05@users.noreply.github.com>
2025-05-08 05:16:11 +0500

    Update q2.asm

--- Labs/10/q2.asm
+++ Labs/10/q2.asm
@@ -1,1 +1,65 @@
+INCLUDE Irvine32.inc
 
+Scan_String PROTO
+
+.data
+    Str1 BYTE "127&j~3#^&*#*#45^", 0
+    foundMsg BYTE "Character found at index: ", 0
+    notFoundMsg BYTE "Character not found", 0
+    index DWORD ?
+
+.code
+
+main PROC
+    push '#'                 
+    push OFFSET Str1         
+    call Scan_String         
+    mov index, eax
+
+    cmp eax, -1
+    je NotFound
+
+    mov edx, OFFSET foundMsg
+    call WriteString
+    call WriteDec
+    call Crlf
+    jmp Done
+
+NotFound:
+    mov edx, OFFSET notFoundMsg
+    call WriteString
+    call Crlf
+
+Done:
+    exit
+main ENDP
+
+Scan_String PROC
+    push ebp
+    mov ebp, esp
+    push edi
+    push ecx
+
+    mov edi, [ebp+8]             
+    mov al, [ebp+12]             
+    mov ecx, 17                  
+    cld
+    repne scasb
+    jz Found
+
+    mov eax, -1
+    jmp Cleanup
+
+Found:
+    sub edi, [ebp+8]
+    dec edi
+    mov eax, edi
+
+Cleanup:
+    pop ecx
+    pop edi
+    pop ebp
+    ret 8
+Scan_String ENDP
+
+END main
