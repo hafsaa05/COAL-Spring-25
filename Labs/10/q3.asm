d74b50a09a130b0ece8b25b625c47f4cfdf8dbc0
Hafsa Rashid <142868426+hafsaa05@users.noreply.github.com>
2025-05-08 05:19:19 +0500

    Update q3.asm

--- Labs/10/q3.asm
+++ Labs/10/q3.asm
@@ -1,1 +1,70 @@
+INCLUDE Irvine32.inc
+
+IsCompare PROTO
+
+.data
+    Str1 BYTE "Hello", 0
+    Str2 BYTE "Hello", 0
+    resultMsg BYTE "Strings are equal", 0
+    notEqualMsg BYTE "Strings are not equal", 0
+
+.code
+
+main PROC
+    push OFFSET Str2        
+    push OFFSET Str1        
+    call IsCompare         
+    cmp eax, 0              
+    je Equal
+    mov edx, OFFSET notEqualMsg
+    call WriteString
+    call Crlf
+    jmp Done
+
+Equal:
+    mov edx, OFFSET resultMsg
+    call WriteString
+    call Crlf
+
+Done:
+    exit
+main ENDP
+
+IsCompare PROC
+    push ebp
+    mov ebp, esp
+    push edi
+    push esi
+    push ecx
+
+    mov esi, [ebp+8]         
+    mov edi, [ebp+12]        
+
+CompareLoop:
+    mov al, [esi]            
+    mov bl, [edi]            
+    cmp al, bl               
+    jne NotEqual             
+    test al, al              
+    jz EqualStrings          
+    inc esi                  
+    inc edi                  
+    jmp CompareLoop
+
+NotEqual:
+    mov eax, 1               
+    jmp Cleanup
+
+EqualStrings:
+    mov eax, 0               
+
+Cleanup:
+    pop ecx
+    pop esi
+    pop edi
+    pop ebp
+    ret 8
+IsCompare ENDP
+
+END main
 
